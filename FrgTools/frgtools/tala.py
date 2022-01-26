b1395359a5e9166075bf70e5e8d4dadf2294f799
TalaSidawi <talasidawi@gmail.com>
2022-01-26 15:41:51 -0800

    fixed tabbing

--- FrgTools/frgtools/tala.py
+++ FrgTools/frgtools/tala.py
@@ -54,38 +54,38 @@ def mini_bff(wavelengths, reflectance, polymer_type, sampleName, plot = False):
 
 
 def water_calibration(ratio, architecture, polymer):
-    #glassglass
-#         y_406 = 0.02x + 0.016
-#         y_806 = 0.015x + 0.03
-#         y_TF4 = 0.009x + 0.013
-#         y_TF8 = -0.002x + 0.024
-    #glassbs
-#         y_406 = 0.023x + 0.069
-#         y_806 = 0.031x + 0.216
-#         y_TF4 = 0.039x + 0.026
-#         y_TF8 = 0.071x + 0.096
+	#glassglass
+	#         y_406 = 0.02x + 0.016
+	#         y_806 = 0.015x + 0.03
+	#         y_TF4 = 0.009x + 0.013
+	#         y_TF8 = -0.002x + 0.024
+	#glassbs
+	#         y_406 = 0.023x + 0.069
+	#         y_806 = 0.031x + 0.216
+	#         y_TF4 = 0.039x + 0.026
+	#         y_TF8 = 0.071x + 0.096
 
-    glassglassdict = {
-        '406': [0.02,0.016],
-        '806': [0.015,0.03],
-        'TF4': [0.009,0.013],
-        'TF8': [-0.002,0.024]
-    }
+	glassglassdict = {
+	'406': [0.02,0.016],
+	'806': [0.015,0.03],
+	'TF4': [0.009,0.013],
+	'TF8': [-0.002,0.024]
+	}
 
 
-    glassbsdict = {
-        '406': [0.024,0.058],
-        '806': [0.03,0.223],
-        'TF4': [0.072,0.011],
-        'TF8': [0.104,0.122]
-    }
+	glassbsdict = {
+	'406': [0.024,0.058],
+	'806': [0.03,0.223],
+	'TF4': [0.072,0.011],
+	'TF8': [0.104,0.122]
+	}
     
-    if architecture == 'GPOLYG':
-        x = (ratio - glassglassdict[polymer][1])/glassglassdict[polymer][0]
-        return x
-    elif architecture == 'GPOLYBS':
-        x = (ratio - glassbsdict[polymer][1])/glassbsdict[polymer][0]
-        return x
+	if architecture == 'GPOLYG':
+		x = (ratio - glassglassdict[polymer][1])/glassglassdict[polymer][0]
+		return x
+	elif architecture == 'GPOLYBS':
+		x = (ratio - glassbsdict[polymer][1])/glassbsdict[polymer][0]
+        	return x
 
 from tqdm import tqdm
 def bscorrect_linescans(df):
@@ -95,19 +95,14 @@ def bscorrect_linescans(df):
 	gaussian_fit_list = []
 
 	for _,row in tqdm(df.iterrows()):
-	    for ii in row['r']:
-		corrected, baseline= mini_bff(row['wl'], ii, 'EVA', row['label'], plot = False)
-
-		correctedAbs_list.append(corrected)
-		baselineAbs_list.append(baseline)
-		#         best_fit = list(curves[row['label']]['Best_Fit']['Abs'])
-		#         gaussian_fit_list.append(best_fit)
+		for ii in row['r']:
+			corrected, baseline = mini_bff(row['wl'], ii, 'EVA', row['label'], plot = False)
+			correctedAbs_list.append(corrected)
+			baselineAbs_list.append(baseline)
 	
 	s = []
-
 	for i in range(len(df)):
-	    s.append(len(df['position'][0])*i)
-
+		s.append(len(df['position'][0])*i)
 	print(s)
 	
 	
@@ -117,30 +112,30 @@ def bscorrect_linescans(df):
 	wardRatio_dict = {}
 
 	if df['polymer'][0] == 'EVA':
-	    pol = '406'
+		pol = '406'
 	else:
-	    pol = 'TF4'
+		pol = 'TF4'
 
 	for i in range(len(s)):
-	    x = i+1
+		x = i+1
 
-	    wardRatio_dict[i] = []
+		wardRatio_dict[i] = []
 
-	    if i+1 >= len(s):
-		correctedAbs_dict[i] = correctedAbs_list[s[i]:]
-		baselineAbs_dict[i] = baselineAbs_list[s[i]:]
-	#         gaussianfit_dict[i] = gaussian_fit_list[s[i]:]
-	    else:
-		correctedAbs_dict[i] = correctedAbs_list[s[i]:s[x]]
-		baselineAbs_dict[i] = baselineAbs_list[s[i]:s[x]]
-	#         gaussianfit_dict[i] = gaussian_fit_list[s[i]:s[x]]
+		if i+1 >= len(s):
+			correctedAbs_dict[i] = correctedAbs_list[s[i]:]
+			baselineAbs_dict[i] = baselineAbs_list[s[i]:]
+			#         gaussianfit_dict[i] = gaussian_fit_list[s[i]:]
+		else:
+			correctedAbs_dict[i] = correctedAbs_list[s[i]:s[x]]
+			baselineAbs_dict[i] = baselineAbs_list[s[i]:s[x]]
+			#         gaussianfit_dict[i] = gaussian_fit_list[s[i]:s[x]]
 
-	    for pos in range(len(df['r'][0])):
-	#         wardRatio_dict[i].append(cali_406(gaussianfit_dict[i][pos][102]/gaussianfit_dict[i][pos][15]))
-		wardRatio_dict[i].append(water_calibration(
-							correctedAbs_dict[i][pos][102]/correctedAbs_dict[i][pos][15],
-							  df['architecture'][0],
-							    pol
-							    )
-							)
+		for pos in range(len(df['r'][0])):
+			#         wardRatio_dict[i].append(cali_406(gaussianfit_dict[i][pos][102]/gaussianfit_dict[i][pos][15]))
+			wardRatio_dict[i].append(water_calibration(
+								correctedAbs_dict[i][pos][102]/correctedAbs_dict[i][pos][15],
+								  df['architecture'][0],
+								    pol
+								    )
+								)
 	return correctedAbs_dict, baselineAbs_dict, wardRatio_dict
